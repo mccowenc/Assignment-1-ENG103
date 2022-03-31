5f05251efde6dcb993507b2d091516c7785e6075
caden <mccowenc@oregonstate.edu>
2022-03-30 19:05:44 -0700

    Assignment 1

--- animal.py
+++ animal.py
@@ -3,5 +3,5 @@
 # Description: Asks the user for their favorite animal and then
 #              prints out that that is their favorite animal
 
-fave_animal = input("Salmon")
+fave_animal = input("Please enter your favorite animal:")
 print("Your favorite animal is the " + fave_animal + ".")
