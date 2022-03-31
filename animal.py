5e6e025c9e0c125049cebb3d277412b5246b0650
caden <mccowenc@oregonstate.edu>
2022-03-30 18:53:00 -0700

    Assignment 1

---
+++ animal.py
@@ -0,0 +1,7 @@
+# GitHub username: mccowenc
+# Date: 3/30/2022
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Salmon")
+print("Your favorite animal is the " + fave_animal + ".")
